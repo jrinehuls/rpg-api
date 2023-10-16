d3748c5fc5b5a3b3297c4e5af2bee9f0d527752a
Justin Rinehuls <jrinehuls@gmail.com>
2023-10-15 20:33:50 -0400

    Initial commit

---
+++ src/test/java/com/jrinehuls/rpgapi/RpgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrinehuls.rpgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
