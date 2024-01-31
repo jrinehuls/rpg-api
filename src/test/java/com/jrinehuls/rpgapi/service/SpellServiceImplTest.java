702792c1c6881507498f8bc77310a52d8572abf9
Justin Rinehuls <jrinehuls@gmail.com>
2024-01-30 20:53:07 -0500

    I wrote a test!

---
+++ src/test/java/com/jrinehuls/rpgapi/service/SpellServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.jrinehuls.rpgapi.service;
+
+import com.jrinehuls.rpgapi.dto.spell.SpellResponseDto;
+import com.jrinehuls.rpgapi.entity.Spell;
+import com.jrinehuls.rpgapi.exception.notfound.SpellNotFoundException;
+import com.jrinehuls.rpgapi.repository.SpellRepository;
+import com.jrinehuls.rpgapi.service.impl.SpellServiceImpl;
+import com.jrinehuls.rpgapi.util.mapper.SpellMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SpellServiceImplTest {
+
+    @Mock
+    private SpellRepository spellRepository;
+
+    @Mock
+    private SpellMapper spellMapper;
+
+    @InjectMocks
+    private SpellServiceImpl spellService;
+
+    @Test
+    void getSpellTest() {
+        Long id = 0L;
+        Spell spell = new Spell();
+        when(spellRepository.findById(id)).thenReturn(Optional.of(spell));
+
+        spellService.getSpell(id);
+
+        verify(spellMapper, times(1)).mapSpellToDto(spell);
+    }
+
+}
