1eb986963144d5d6e7bc9c0fc399a9049f41437f
Justin Rinehuls <jrinehuls@gmail.com>
2024-04-30 16:34:40 -0400

    Add API responses for users

--- src/main/java/com/jrinehuls/rpgapi/controller/UserController.java
+++ src/main/java/com/jrinehuls/rpgapi/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.jrinehuls.rpgapi.controller;
 
+import com.jrinehuls.rpgapi.dto.spell.SpellResponseDto;
 import com.jrinehuls.rpgapi.dto.user.UserDto;
+import com.jrinehuls.rpgapi.exception.ErrorResponse;
 import com.jrinehuls.rpgapi.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +21,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user")
 @AllArgsConstructor
 @CrossOrigin("*")
+@Tag(name = "Spell Controller", description = "Endpoints for managing spells")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/register")
+    // Post User
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful creation of user",
+                    content = @Content(schema = @Schema(implementation = UserDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid payload",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Conflict: username already in use",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @Operation(summary = "Register user", description = "For new users to register")
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDto> registerUser(@Valid @RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.registerUser(userDto), HttpStatus.CREATED);
     }
